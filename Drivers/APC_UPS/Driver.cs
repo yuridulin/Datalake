1ebed50e0ff127e2cec3feb37027187bff7a458c
Юрий Дулин <y.dulin@vitebsk.energo.net>
2020-10-29 08:48:53 +0300

    В драйвер APC добавлен рестарт процесса apcupsd и оповещения в журнал Windows о критических изменениях

--- Drivers/APC_UPS/Driver.cs
+++ Drivers/APC_UPS/Driver.cs
@@ -5,10 +5,11 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
+using iNOPC.Drivers.APC_UPS;
 using iNOPC.Library;
 using Newtonsoft.Json;
 
-namespace iNOPC.Drivers.APC_UPS
+namespace iNOPC.Drivers.APC
 {
     public class Driver : IDriver
     {
@@ -23,72 +24,6 @@ namespace iNOPC.Drivers.APC_UPS
         public bool Start(string jsonConfig)
         {
             LogEvent("Запуск ...");
-            Fields = new Dictionary<string, object>
-            {
-                { "Time", "" },
-                { "APC", "" },
-                { "HOSTNAME", "" },
-                { "RELEASE", "" },
-                { "VERSION", "" },
-                { "UPSNAME", "" },
-                { "CABLE", "" },
-                { "MODEL", "" },
-                { "UPSMODE", "" },
-                { "STARTTIME", "" },
-                { "STATUS", "" },
-                { "MBATTCHG", "" },
-                { "MINTIMEL", "" },
-                { "MAXTIME", "" },
-                { "MAXLINEV", "" },
-                { "MINLINEV", "" },
-                { "SENSE", "" },
-                { "DWAKE", "" },
-                { "DSHUTD", "" },
-                { "DLOWBATT", "" },
-                { "LOTRANS", "" },
-                { "HITRANS", "" },
-                { "RETPCT", "" },
-                { "ALARMDEL", "" },
-                { "LINEFREQ", "" },
-                { "LASTXFER", "" },
-                { "NUMXFERS", "" },
-                { "TONBATT", "" },
-                { "CUMONBATT", "" },
-                { "XOFFBATT", "" },
-                { "SELFTEST", "" },
-                { "STESTI", "" },
-                { "STATFLAG", "" },
-                { "DIPSW", "" },
-                { "REG1", "" },
-                { "REG2", "" },
-                { "REG3", "" },
-                { "SERIALNO", "" },
-                { "NOMOUTV", "" },
-                { "NOMBATTV", "" },
-                { "EXTBATTS", "" },
-                { "BADBATTS", "" },
-                { "FIRMWARE", "" },
-                { "APCMODEL", "" },
-                { "CURRENT", "" },
-                { "LOWRANS", "" },
-                { "DRIVER", "" },
-                { "XONBATT", "" },
-                { "NOMPOWER", "" },
-                { "LASTSTEST", "" },
-
-                { "LINEV", 0 },
-                { "LOADPCT", 0 },
-                { "BCHARGE", 0 },
-                { "TIMELEFT", 0 },
-                { "OUTPUTV", 0 },
-                { "ITEMP", 0 },
-                { "BATTV", 0 },
-
-                { "DATE", DateTime.Now },
-                { "MANDATE", DateTime.Now },
-                { "BATTDATE", DateTime.Now },
-                { "END APC", DateTime.Now },
-            };
 
             // чтение конфигурации
             try
@@ -98,6 +33,17 @@ namespace iNOPC.Drivers.APC_UPS
             catch (Exception e)
             {
                 LogEvent("Конфигурация не прочитана: " + e.Message, LogType.ERROR);
+                return false;
+            }
+
+            // Определение имени процесс ApcUpsD
+            try
+			{
+                ExeName = Configuration.Exe.Substring(Configuration.Exe.LastIndexOf('\\') + 1).Replace(".exe", "");
+            }
+            catch (Exception e)
+            {
+                LogEvent("Не удалось определить имя процесса ApcUpsD по строке \"" + Configuration.Exe + "\": " + e.Message, LogType.ERROR);
                 return false;
             }
 
@@ -135,13 +81,58 @@ namespace iNOPC.Drivers.APC_UPS
 
         Timer Timer { get; set; }
 
-        private string[] DateTimeFields { get; set; } = new string[] { "DATE", "XONBATT", "TONBATT", "XOFFBATT", "LASTSTEST", "END APC", "MANDATE", "BATTDATE" };
+        Stats Stats { get; set; } = new Stats { ACTIVE = false, DATE = DateTime.Today };
 
-        private string[] NumberFields { get; set; } = new string[] { "LINEV", "LOADPCT", "BCHARGE", "TIMELEFT", "OUTPUTV", "ITEMP", "BATTV" };
+        string ExeName { get; set; }
+
+
+        DateTime ToDate(string text)
+		{
+			text = text
+				.Replace("╠шэёъюх тЁхь", "")
+				.Replace("Минское время", "")
+				.Replace("Калининградское время", "")
+				.Replace("(зима)", "")
+				.Replace("RTZ 2", "")
+				.Replace("Seconds", "")
+				//.Replace("seconds", "")
+				.Replace("+0300", "")
+				.Replace("(чшьр)", "")
+				.Replace("  ", " ")
+				.Trim();
+
+			if (DateTime.TryParse(text, out DateTime dt)) return dt;
+            else if (DateTime.TryParseExact(text, "ddd MMM dd HH:mm:ss yyyy", CultureInfo.GetCultureInfo("en-US"), DateTimeStyles.None, out dt)) return dt;
+            else if (DateTime.TryParseExact(text, "ddd MMM dd HH:mm:ss yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt)) return dt;
+            else if (DateTime.TryParseExact(text, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt)) return dt;
+            else if (DateTime.TryParseExact(text, "MM/dd/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt)) return dt;
+            else return new DateTime(1, 1, 1);
+        }
+
+        double ToNumeric(string text)
+		{
+            string[] vs = text.Split(' ');
+            foreach (string s in vs)
+            {
+                if (double.TryParse(s.Replace(".", ","), out double d))
+                {
+                    return d;
+                }
+            }
+
+            return 0;
+        }
 
         void LoadData()
         {
-            var lines = new string[0];
+            // Проверка, запущен ли процесс ApcUpsD. Если нет - процесс запускается по указанному в конфиге пути
+            var processes = Process.GetProcesses().Where(x => x.ProcessName == ExeName);
+            if (processes.Count() < 1)
+            {
+                Process.Start(Configuration.Exe);
+                LogEvent("Перезапуск процесса ApcUpsD \"" + Configuration.Exe + "\"", LogType.ERROR);
+                WinLogEvent("Перезапуск процесса ApcUpsD \"" + Configuration.Exe + "\"");
+            }
 
             // Создаем подключение к серверу данных и читаем вывод из консоли
             string raw = "";
@@ -151,7 +142,7 @@ namespace iNOPC.Drivers.APC_UPS
                 {
                     cmd.StartInfo = new ProcessStartInfo
                     {
-                        FileName = Configuration.Path,
+                        FileName = Environment.CurrentDirectory + @"\Drivers\ApcAccess\ApcAccess.exe",
                         Arguments = "status " + Configuration.Url,
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
@@ -165,17 +156,19 @@ namespace iNOPC.Drivers.APC_UPS
             catch (Exception)
             {
                 LogEvent("Источник данных не отвечает", LogType.ERROR);
+                LoadError();
                 return;
             }
 
             if (raw == "")
             {
                 LogEvent("Источник данных не предоставляет данных для обработки", LogType.ERROR);
+                LoadError();
                 return;
             }
 
-
             // обрабатываем полученный вывод, деля построчно
+            var lines = new string[0];
             try
             {
                 lines = raw.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
@@ -183,93 +176,177 @@ namespace iNOPC.Drivers.APC_UPS
             catch (Exception e)
             {
                 LogEvent("Ошибка при разделении строк [" + e.Message + "]", LogType.ERROR);
+                LoadError();
                 return;
             }
 
-            lock (Fields)
+            var stats = new Stats();
+
+            foreach (string line in lines)
             {
-                foreach (string line in lines)
+                if (line.Contains(":"))
                 {
-                    // каждую строку разбиваем на пару "ключ" - "значение"
-                    if (line.Contains(":"))
+                    try
                     {
-                        try
+                        // разбиваем строку на пару "ключ" - "значение"
+                        int i = line.IndexOf(':');
+                        string name = line.Substring(0, i - 1).Trim().ToUpper();
+                        string val = line.Substring(i + 1).Trim();
+
+                        // получаем нужные значения
+                        switch (name)
                         {
-                            int i = line.IndexOf(':');
-                            string name = line.Substring(0, i - 1).Trim().ToUpper();
-                            string val = line.Substring(i + 1).Trim();
+                            case "STATUS":
+                                stats.STATUS = val;
+                                break;
 
-                            if (NumberFields.Contains(name))
-                            {
-                                string[] vs = val.Split(' ');
-                                foreach (string s in vs)
-                                {
-                                    if (float.TryParse(s.Replace(".", ","), out float f))
-                                    {
-                                        Fields[name] = f.ToString().Replace(',', '.');
-                                        break;
-                                    }
-                                }
-                            }
+                            case "SELFTEST":
+                                stats.SELFTEST = val;
+                                break;
 
-                            else if (DateTimeFields.Contains(name))
-                            {
-                                val = val
-                                    .Replace("╠шэёъюх тЁхь", "")
-                                    .Replace("Минское время", "")
-                                    .Replace("Калининградское время", "")
-                                    .Replace("(зима)", "")
-                                    .Replace("RTZ 2", "")
-                                    .Replace("Seconds", "")
-                                    //.Replace("seconds", "")
-                                    .Replace("+0300", "")
-                                    .Replace("(чшьр)", "")
-                                    .Replace("  ", " ")
-                                    .Trim();
+                            case "LASTXFER":
+                                stats.LASTXFER = val;
+                                break;
 
-                                //Thu Oct 01 10:54:45 2020
-                                if (DateTime.TryParse(val, out DateTime d))
-                                {
-                                    Fields[name] = d.ToString("dd.MM.yyyy HH:mm:ss");
-                                }
-                                else if (DateTime.TryParseExact(val, "ddd MMM dd HH:mm:ss yyyy", CultureInfo.GetCultureInfo("en-US"), DateTimeStyles.None, out d))
-                                {
-                                    Fields[name] = d.ToString("dd.MM.yyyy HH:mm:ss");
-                                }
-                                else if (DateTime.TryParseExact(val, "ddd MMM dd HH:mm:ss yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out d))
-                                {
-                                    Fields[name] = d.ToString("dd.MM.yyyy HH:mm:ss");
-                                }
-                                else if (DateTime.TryParseExact(val, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out d))
-                                {
-                                    Fields[name] = d.ToString("dd.MM.yyyy HH:mm:ss");
-                                }
-                                else if (DateTime.TryParseExact(val, "MM/dd/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out d))
-                                {
-                                    Fields[name] = d.ToString("dd.MM.yyyy HH:mm:ss");
-                                }
-                                else
-								{
-                                    LogEvent("Строка с датой не разобрана: " + val, LogType.WARNING);
-                                    Fields[name] = val;
-                                }
-                            }
+                            case "DATE":
+                                stats.DATE = ToDate(val);
+                                break;
 
-                            else if (Fields.ContainsKey(name))
-                            {
-                                Fields[name] = string.IsNullOrEmpty(val) ? null : val;
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            LogEvent("Строка не разобрана: " + line + "\n" + e.Message + "\n" + e.StackTrace, LogType.WARNING);
+                            case "LASTSTEST":
+                                stats.LASTSTEST = val;
+                                break;
+
+                            case "TONBATT":
+                                stats.TONBATT = val;
+                                break;
+
+                            case "XONBATT":
+                                stats.XONBATT = val;
+                                break;
+
+                            case "XOFFBATT":
+                                stats.XOFFBATT = val;
+                                break;
+
+                            case "BCHARGE":
+                                stats.BCHARGE = ToNumeric(val);
+                                break;
+
+                            case "LOADPCT":
+                                stats.LOADPCT = ToNumeric(val);
+                                break;
+
+                            case "ITEMP":
+                                stats.ITEMP = ToNumeric(val);
+                                break;
+
+                            case "LINEV":
+                                stats.LINEV = ToNumeric(val);
+                                break;
+
+                            case "OUTPUTV":
+                                stats.OUTPUTV = ToNumeric(val);
+                                break;
+
+                            case "TIMELEFT":
+                                stats.TIMELEFT = ToNumeric(val);
+                                break;
                         }
                     }
+                    catch (Exception e)
+                    {
+                        LogEvent("Строка не разобрана: " + line + "\n" + e.Message + "\n" + e.StackTrace, LogType.WARNING);
+                    }
                 }
-
-                Fields["Time"] = DateTime.Now.ToString("HH:mm:ss");
             }
-            
+
+            stats.ACTIVE = (DateTime.Now - stats.DATE).TotalMinutes < 2;
+
+            // Проверка уставок по значимым полям
+            if (stats.STATUS != Stats.STATUS)
+			{
+                Log("Статус изменился с [" + Stats.STATUS + "] на [" + stats.STATUS + "]");
+			}
+            if (stats.ACTIVE != Stats.ACTIVE)
+			{
+                if (stats.ACTIVE)
+				{
+                    Log("Восстановлена связь со службой ApcUpsD");
+                }
+                else
+				{
+                    Log("Служба ApcUpsD не отвечает");
+                }
+            }
+            if (stats.LINEV < 200)
+			{
+                if (stats.LINEV != Stats.LINEV)
+				{
+                    Log("Напряжение меньше 200 V");
+                }
+			} 
+            else if (Stats.LINEV < 200)
+            {
+                Log("Напряжение в норме");
+            }
+            if (stats.ITEMP > 40)
+            {
+                if (stats.ITEMP != Stats.ITEMP)
+                {
+                    Log("Температура больше 40 С");
+                }
+            }
+            else if (Stats.ITEMP > 40)
+            {
+                Log("Температура в норме");
+            }
+            if (stats.TIMELEFT < 30)
+            {
+                if (stats.ITEMP != Stats.ITEMP)
+                {
+                    Log("Время до отключения меньше 30 минут");
+                }
+            }
+            else if (Stats.ITEMP < 30)
+            {
+                Log("Время до отключения в норме");
+            }
+
+            // Обновление OPC полей
+            lock (Fields)
+            {
+                Fields["Time"] = DateTime.Now.ToString("HH:mm:ss");
+                Fields["ACTIVE"] = stats.ACTIVE;
+                Fields["STATUS"] = stats.STATUS;
+                Fields["SELFTEST"] = stats.SELFTEST;
+                Fields["LASTXFER"] = stats.LASTXFER;
+                Fields["DATE"] = stats.DATE.ToString("dd.MM.yyyy HH:mm:ss");
+                Fields["LASTSTEST"] = stats.LASTSTEST;
+                Fields["TONBATT"] = stats.TONBATT;
+                Fields["XOFFBATT"] = stats.XOFFBATT;
+                Fields["XONBATT"] = stats.XONBATT;
+                Fields["BCHARGE"] = stats.BCHARGE;
+                Fields["LOADPCT"] = stats.LOADPCT;
+                Fields["ITEMP"] = stats.ITEMP;
+                Fields["LINEV"] = stats.LINEV;
+                Fields["OUTPUTV"] = stats.OUTPUTV;
+                Fields["TIMELEFT"] = stats.TIMELEFT;
+            }
+
+            UpdateEvent();
+            Stats = stats;
+        }
+
+        void Log(string message)
+		{
+            LogEvent(message);
+            EventLog.WriteEntry("apcupds", "UPS \"" + Configuration.Name + "\": " + message);
+        }
+
+        void LoadError()
+		{
+            Fields["ACTIVE"] = Stats.ACTIVE = false;
+            Fields["Time"] = DateTime.Now.ToString("HH:mm:ss");
             UpdateEvent();
         }
     }
---
+++ Drivers/APC_UPS/Stats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace iNOPC.Drivers.APC_UPS
+{
+	public class Stats
+	{
+		public bool ACTIVE { get; set; }
+
+		public DateTime DATE { get; set; }
+
+		public string STATUS { get; set; }
+
+		public string SELFTEST { get; set; }
+
+		public string LASTXFER { get; set; }
+
+		public string LASTSTEST { get; set; }
+
+		public string TONBATT { get; set; }
+
+		public string XOFFBATT { get; set; }
+
+		public string XONBATT { get; set; }
+
+		public double BCHARGE { get; set; }
+
+		public double LOADPCT { get; set; }
+
+		public double ITEMP { get; set; }
+
+		public double LINEV { get; set; }
+
+		public double OUTPUTV { get; set; }
+
+		public double TIMELEFT { get; set; }
+	}
+}
