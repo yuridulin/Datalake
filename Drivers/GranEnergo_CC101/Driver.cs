3f4d4c8e9bee1ddaf10f6321703d776915e81d68
Юрий Дулин <y.dulin@vitebsk.energo.net>
2021-10-14 14:15:25 +0300

    Добавлена защита от нулевого ответа, правки механизма таймера

--- Drivers/GranEnergo_CC101/Driver.cs
+++ Drivers/GranEnergo_CC101/Driver.cs
@@ -98,6 +98,8 @@ namespace GranEnergo_CC101
 
 		DateTime LastDay { get; set; } = DateTime.MinValue;
 
+		DateTime LastMonth { get; set; } = DateTime.MinValue;
+
 		void Exchange()
 		{
 			if (!IsDriverActive) return;
@@ -106,24 +108,26 @@ namespace GranEnergo_CC101
 			IsExchangeRunning = true;
 
 			// определение необходимости опроса прибора
-			bool needCurrent = false, needDay = false;
+			bool needCurrent = false, needDay = false, needMonth = false;
 			DateTime now = DateTime.Now;
 
 			if (Configuration.CurrentValuesInterval > 0 && (now - LastCurrent).TotalSeconds > Configuration.CurrentValuesInterval)
 			{
-				LastCurrent = now;
 				needCurrent = true;
 				LogEvent("Чтение текущих значений", LogType.DETAILED);
 			}
-			if (now.Date != LastDay.Date && now.TimeOfDay >= DateTime.Parse("01.01.2000 03:00:00").TimeOfDay)
+			if (Configuration.CheckDayValues && now.Date != LastDay.Date && now.TimeOfDay >= DateTime.Parse("01.01.2000 " + now.Hour + ":03:00").TimeOfDay)
 			{
-				LastDay = now;
 				needDay = true;
-				if (Configuration.CheckDayValues) LogEvent("Чтение значений за сутки", LogType.DETAILED);
-				if (Configuration.CheckMonthValues) LogEvent("Чтение значений за месяц", LogType.DETAILED);
+				LogEvent("Чтение значений за сутки", LogType.DETAILED);
+			}
+			if (Configuration.CheckMonthValues && now.Date != LastMonth.Date && now.TimeOfDay >= DateTime.Parse("01.01.2000 00:03:00").TimeOfDay)
+			{
+				needMonth = true;
+				LogEvent("Чтение значений за месяц", LogType.DETAILED);
 			}
 
-			if (!needCurrent && !needDay)
+			if (!needCurrent && !needDay && !needMonth)
 			{
 				IsExchangeRunning = false;
 				lock (Fields)
@@ -163,27 +167,36 @@ namespace GranEnergo_CC101
 				if (needCurrent)
 				{
 					// все мгновенные значения
-					b = ReadAndWrite(new byte[] { 0x00, 0x04, 0x2E, 0x00, 0x00, 0x01, 0x33, 0x39 }, 30);
-					Value("Current.P", BitConverter.ToSingle(new byte[] { b[4], b[5], b[6], b[7], }, 0));
-					Value("Current.Q", BitConverter.ToSingle(new byte[] { b[8], b[9], b[10], b[11], }, 0));
-					Value("Current.U", BitConverter.ToSingle(new byte[] { b[12], b[13], b[14], b[15], }, 0));
-					Value("Current.I", BitConverter.ToSingle(new byte[] { b[16], b[17], b[18], b[19], }, 0));
-					Value("Current.kP", BitConverter.ToSingle(new byte[] { b[20], b[21], b[22], b[23], }, 0));
-					Value("Current.F", BitConverter.ToSingle(new byte[] { b[24], b[25], b[26], b[27], }, 0));
+					if (ReadAndWrite(new byte[] { 0x00, 0x04, 0x2E, 0x00, 0x00, 0x01, 0x33, 0x39 }, 30, out b))
+					{
+						Value("Current.P", BitConverter.ToSingle(new byte[] { b[4], b[5], b[6], b[7], }, 0));
+						Value("Current.Q", BitConverter.ToSingle(new byte[] { b[8], b[9], b[10], b[11], }, 0));
+						Value("Current.U", BitConverter.ToSingle(new byte[] { b[12], b[13], b[14], b[15], }, 0));
+						Value("Current.I", BitConverter.ToSingle(new byte[] { b[16], b[17], b[18], b[19], }, 0));
+						Value("Current.kP", BitConverter.ToSingle(new byte[] { b[20], b[21], b[22], b[23], }, 0));
+						Value("Current.F", BitConverter.ToSingle(new byte[] { b[24], b[25], b[26], b[27], }, 0));
+						LastCurrent = now;
+					}
 				}
 
 				if (needDay && Configuration.CheckDayValues)
 				{
 					// накопленная энергия за последние сутки
-					b = ReadAndWrite(new byte[] { 0x00, 0x03, 0x2A, 0x00, 0x00, 0x00, 0x03, 0x4C }, 22);
-					Value("LastDay.E", 7.5 / 10000 * BitConverter.ToInt32(new byte[] { b[4], b[5], b[6], b[7] }, 0));
+					if (ReadAndWrite(new byte[] { 0x00, 0x03, 0x2A, 0x00, 0x00, 0x00, 0x03, 0x4C }, 22, out b))
+					{
+						Value("LastDay.E", 7.5 / 10000 * BitConverter.ToInt32(new byte[] { b[4], b[5], b[6], b[7] }, 0));
+						LastDay = now;
+					}
 				}
 
-				if (needDay && Configuration.CheckMonthValues)
+				if (needMonth && Configuration.CheckMonthValues)
 				{
 					// накопленная энергия за последний месяц
-					b = ReadAndWrite(new byte[] { 0x00, 0x03, 0x2B, 0x00, 0x00, 0x00, 0x03, 0x4C }, 22);
-					Value("LastMonth.E", 7.5 / 10000 * BitConverter.ToInt32(new byte[] { b[4], b[5], b[6], b[7] }, 0));
+					if (ReadAndWrite(new byte[] { 0x00, 0x03, 0x2B, 0x00, 0x00, 0x00, 0xFF, 0x4D }, 22, out b))
+					{
+						Value("LastMonth.E", 7.5 / 10000 * BitConverter.ToInt32(new byte[] { b[4], b[5], b[6], b[7] }, 0));
+						LastMonth = now;
+					}
 				}
 
 				// идентификационный номер
@@ -224,7 +237,7 @@ namespace GranEnergo_CC101
 				UpdateEvent();
 			}
 
-			byte[] ReadAndWrite(byte[] command, int size)
+			bool ReadAndWrite(byte[] command, int size, out byte[] answer)
 			{
 				stream.Write(command, 0, command.Length);
 				LogEvent("TX: " + Helpers.BytesToString(command), LogType.DETAILED);
@@ -235,7 +248,8 @@ namespace GranEnergo_CC101
 				stream.Read(buffer, 0, buffer.Length);
 				LogEvent("RX: " + Helpers.BytesToString(buffer), LogType.DETAILED);
 
-				return buffer;
+				answer = buffer;
+				return buffer[size - 1] != 0x00 && buffer[size - 2] != 0x00;
 			}
 
 			void Value(string name, object value)
