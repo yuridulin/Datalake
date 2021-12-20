daab5f8472b47a317b025e67ab5dff4e6b7bf621
Юрий Дулин <y.dulin@vitebsk.energo.net>
2021-12-20 09:12:30 +0300

    Драйвер замыкания COM порта

---
+++ Drivers/COM_Echo/Driver.cs
@@ -0,0 +1,158 @@
+﻿using iNOPC.Library;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace COM_Echo
+{
+	public class Driver : IDriver
+	{
+        public Dictionary<string, DefField> Fields { get; set; } = new Dictionary<string, DefField>();
+
+		public string Version { get; } = typeof(Driver).Assembly.GetName().Version.ToString();
+
+		public event LogEvent LogEvent;
+		public event UpdateEvent UpdateEvent;
+
+		public bool Start(string jsonConfiguration)
+		{
+            LogEvent("Запуск ...");
+
+            try { Port?.Close(); } catch { }
+
+            CreateFields();
+
+            try
+            {
+                Configuration = JsonConvert.DeserializeObject<Configuration>(jsonConfiguration);
+            }
+            catch (Exception e)
+            {
+                return Err("Конфигурация не прочитана: " + e.Message + "\n" + e.StackTrace);
+            }
+
+            try
+            {
+                Port.PortName = Configuration.PortName;
+                Port.BaudRate = Configuration.BaudRate;
+                Port.DataBits = Configuration.DataBits;
+                Port.Parity = (Parity)Configuration.Parity;
+                Port.StopBits = (StopBits)Configuration.StopBits;
+            }
+            catch (Exception e)
+            {
+                return Err("Ошибка при установке параметров COM порта: " + e.Message);
+            }
+
+            Timer.Interval = Configuration.Interval * 1000;
+            Timer.Start();
+
+            Task.Run(Exchange);
+
+            LogEvent("Мониторинг запущен");
+
+            return true;
+        }
+
+		public void Stop()
+		{
+            LogEvent("Остановка ...");
+
+            Timer.Stop();
+            try { Port.Close(); } catch (Exception) { }
+
+            LogEvent("Мониторинг остановлен");
+        }
+
+		public void Write(string fieldName, object value)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		Configuration Configuration { get; set; }
+
+        SerialPort Port { get; set; }
+
+        Timer Timer { get; set; }
+
+        void CreateFields()
+        {
+            lock (Fields)
+            {
+                Fields?.Clear();
+                Fields.Add("Time", new DefField { Value = DateTime.Now.ToString("HH:mm:ss"), Quality = 0 });
+                Fields.Add("Echo", new DefField { Value = false, Quality = 0 });
+            }
+
+            UpdateEvent();
+
+            Port = new SerialPort();
+            Port.ErrorReceived += (s, e) =>
+            {
+                LogEvent("Ошибка COM порта: " + e.EventType.ToString(), LogType.DETAILED);
+                Port.BaseStream.Flush();
+                Port.DiscardInBuffer();
+                Port.DiscardOutBuffer();
+            };
+
+            Timer = new Timer();
+            Timer.Elapsed += (s, e) => Exchange();
+        }
+
+        void Exchange()
+		{
+            try
+            {
+                if (!Port.IsOpen)
+                {
+                    try
+                    {
+                        Port.Open();
+                    }
+                    catch (Exception)
+                    {
+                        Err("errorНе удалось открыть COM порт");
+                        return;
+                    }
+                }
+
+                string date = DateTime.Now.ToString("HH:mm:ss");
+
+                Port.BaseStream.Flush();
+                Port.Write(date);
+                LogEvent("TX: " + date, LogType.DETAILED);
+
+                Task.Delay(Configuration.EchoDelay).Wait();
+
+                string echo = Port.ReadExisting();
+                LogEvent("RX: " + echo + " [" + (date == echo ? "true" : "false") + "]", LogType.DETAILED);
+
+                Fields["Echo"].Value = date == echo;
+                Fields["Time"].Value = DateTime.Now.ToString("HH:mm:ss");
+
+                Fields["Echo"].Quality = 192;
+                Fields["Time"].Quality = 192;
+            }
+            catch (Exception e)
+            {
+                Err("Ошибка при отправке запроса [" + e.Message + "]");
+                Fields["Echo"].Quality = 0;
+                Fields["Time"].Quality = 0;
+            }
+			finally
+			{
+                UpdateEvent();
+			}
+        }
+
+        bool Err(string message)
+		{
+            LogEvent(message, LogType.ERROR);
+            return false;
+		}
+	}
+}
---
+++ Drivers/COM_Echo/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("COM_Echo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("COM_Echo")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4011cfe9-c73b-49fb-bc8f-d5864c7e3c96")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
