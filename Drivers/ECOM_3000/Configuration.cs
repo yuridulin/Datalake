54687c4e22ecbd31a080527561a2167a72ca91ed
Юрий Дулин <y.dulin@vitebsk.energo.net>
2022-07-28 08:24:41 +0300

    Добавлен флаг получения данных и его сброс при ошибках

--- Drivers/ECOM_3000/Configuration.cs
+++ Drivers/ECOM_3000/Configuration.cs
@@ -3,19 +3,19 @@ using Newtonsoft.Json;
 
 namespace iNOPC.Drivers.ECOM_3000
 {
-    public class Configuration
-    {
-        public int Timeout { get; set; } = 60000;
+	public class Configuration
+	{
+		public int Timeout { get; set; } = 60000;
 
-        public string Url { get; set; } = "192.168.0.1:24";
+		public string Url { get; set; } = "192.168.0.1:24";
 
-        public static string GetPage(string json)
-        {
-            var config = JsonConvert.DeserializeObject<Configuration>(json);
+		public static string GetPage(string json)
+		{
+			var config = JsonConvert.DeserializeObject<Configuration>(json);
 
-            return
-                Html.Value("Время между опросами, мс", nameof(config.Timeout), config.Timeout) +
-                Html.Value("Адрес сервера", nameof(config.Url), config.Url);
-        }
-    }
+			return
+				Html.Value("Время между опросами, мс", nameof(config.Timeout), config.Timeout) +
+				Html.Value("Адрес сервера", nameof(config.Url), config.Url);
+		}
+	}
 }
--- Drivers/ECOM_3000/Driver.cs
+++ Drivers/ECOM_3000/Driver.cs
@@ -10,210 +10,267 @@ using System.Timers;
 
 namespace iNOPC.Drivers.ECOM_3000
 {
-    public class Driver : IDriver
-    {
-        public string Version { get; } = typeof(Driver).Assembly.GetName().Version.ToString();
+	public class Driver : IDriver
+	{
+		public string Version { get; } = typeof(Driver).Assembly.GetName().Version.ToString();
 
-        public event LogEvent LogEvent;
+		public event LogEvent LogEvent;
 
-        public event UpdateEvent UpdateEvent;
+		public event UpdateEvent UpdateEvent;
 
-        public Dictionary<string, DefField> Fields { get; set; } = new Dictionary<string, DefField>();
+		public Dictionary<string, DefField> Fields { get; set; } = new Dictionary<string, DefField>();
 
-        public bool Start(string jsonConfig)
-        {
-            LogEvent("Запуск ...");
-            Fields.Clear();
-            Fields.Add("Time", new DefField { Value = DateTime.Now.ToString("HH:mm:ss"), Quality = 192 });
+		public bool Start(string jsonConfig)
+		{
+			LogEvent("Запуск ...");
+			Fields.Clear();
+			Fields.Add("Time", new DefField { Value = DateTime.Now.ToString("HH:mm:ss"), Quality = 0 });
+			Fields.Add("IsActive", new DefField { Value = false, Quality = 0 });
 
-            for (int k = 1; k < 270; k++)
-            {
-                Fields.Add("Hour.B" + k, new DefField { Value = 0, Quality = 192 });
-                Fields.Add("Day.B" + k, new DefField { Value = 0, Quality = 192 });
-            }
+			for (int k = 1; k < 270; k++)
+			{
+				Fields.Add("Hour.B" + k, new DefField { Value = 0, Quality = 0 });
+				Fields.Add("Day.B" + k, new DefField { Value = 0, Quality = 0 });
+			}
 
-            // чтение конфигурации
-            try
-            {
-                Configuration = JsonConvert.DeserializeObject<Configuration>(jsonConfig);
-            }
-            catch (Exception e)
-            {
-                LogEvent("Конфигурация не прочитана: " + e.Message, LogType.ERROR);
-                return false;
-            }
+			// чтение конфигурации
+			try
+			{
+				Configuration = JsonConvert.DeserializeObject<Configuration>(jsonConfig);
+			}
+			catch (Exception e)
+			{
+				LogEvent("Конфигурация не прочитана: " + e.Message, LogType.ERROR);
+				return false;
+			}
 
-            Timer = new Timer(Configuration.Timeout);
-            Timer.Elapsed += (s, e) => LoadData(false);
-            Timer.Start();
+			Timer = new Timer(Configuration.Timeout);
+			Timer.Elapsed += (s, e) => LoadData(false);
+			Timer.Start();
 
-            LogEvent("Мониторинг запущен");
+			LogEvent("Мониторинг запущен");
 
-            Task.Run(() => LoadData(true));
+			Task.Run(() => LoadData(true));
 
-            return true;
-        }
+			return true;
+		}
 
-        public void Stop()
-        {
-            LogEvent("Остановка ...");
+		public void Stop()
+		{
+			LogEvent("Остановка ...");
 
-            Timer.Stop();
+			try { Timer?.Stop(); } catch { }
+			try
+			{
+				lock (Fields)
+				{
+					foreach (var field in Fields.Values)
+					{
+						field.Quality = 0;
+					}
+					Fields["Time"].Value = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+					Fields["Active"].Value = false;
+				}
 
-            LogEvent("Мониторинг остановлен");
-        }
+				UpdateEvent();
+			}
+			catch { }
 
-        public void Write(string fieldName, object value)
-        {
-            // Определение типа записываемого поля
-            LogEvent("Событие записи в поле [" + fieldName + "], значение [" + value + "], тип значения [" + value.GetType() + "]", LogType.DETAILED);
-        }
+			LogEvent("Мониторинг остановлен");
+		}
+
+		public void Write(string fieldName, object value)
+		{
+			// Определение типа записываемого поля
+			LogEvent("Событие записи в поле [" + fieldName + "], значение [" + value + "], тип значения [" + value.GetType() + "]", LogType.DETAILED);
+		}
 
 
-        // Реализация получения данных
+		// Реализация получения данных
 
-        Configuration Configuration { get; set; }
+		Configuration Configuration { get; set; }
 
-        Timer Timer { get; set; }
+		Timer Timer { get; set; }
 
-        void LoadData(bool firstStart)
-        {
-            var date = DateTime.Now;
+		void LoadData(bool firstStart)
+		{
+			var date = DateTime.Now;
 
-            LogEvent("Срабатывание таймера в " + date.ToString("dd.MM.yyyy HH:mm:ss"), LogType.DETAILED);
+			LogEvent("Срабатывание таймера в " + date.ToString("dd.MM.yyyy HH:mm:ss"), LogType.DETAILED);
 
-            try
-            {
-                if (date.Minute == 3 || firstStart)
-                {
-                    string url = Configuration.Url + "&interval=main" + "&t1=" + date.AddHours(-1).ToString("yyyyMMddHH0000.000") + "&t2=" + date.AddHours(-1).ToString("yyyyMMddHH3000.000");
+			try
+			{
+				if (date.Minute == 3 || firstStart)
+				{
+					string url = Configuration.Url + "&interval=main" + "&t1=" + date.AddHours(-1).ToString("yyyyMMddHH0000.000") + "&t2=" + date.AddHours(-1).ToString("yyyyMMddHH3000.000");
 
-                    try
-                    {
-                        string raw = GetHtml(url);
+					try
+					{
+						string raw = GetHtml(url);
 
-                        if (!string.IsNullOrEmpty(raw))
-                        {
-                            lock (Fields)
-                            {
-                                string[] rows = raw.Split('\n');
+						if (!string.IsNullOrEmpty(raw))
+						{
+							lock (Fields)
+							{
+								string[] rows = raw.Split('\n');
 
-                                foreach (var key in Fields.Keys)
-                                    if (key.Contains("Hour.")) Fields[key].Value = 0;
+								foreach (var key in Fields.Keys)
+									if (key.Contains("Hour.")) Fields[key].Value = 0;
 
-                                foreach (string row in rows)
-                                {
-                                    string[] parts = row.Split(',');
+								foreach (string row in rows)
+								{
+									string[] parts = row.Split(',');
 
-                                    if (parts.Length > 2)
-                                    {
-                                        string name = parts[0].Trim();
-                                        float value = float.TryParse(parts[2].Trim().Replace(".", ","), out float f) ? f : 0;
+									if (parts.Length > 2)
+									{
+										string name = parts[0].Trim();
+										float value = float.TryParse(parts[2].Trim().Replace(".", ","), out float f) ? f : 0;
 
-                                        if (Fields.ContainsKey("Hour." + name))
-                                        {
-                                            Fields["Hour." + name].Value = Convert.ToSingle(Fields["Hour." + name].Value) + value;
-                                            Fields["Hour." + name].Quality = 192;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            LogEvent("Нет часовых значений по ЭКОМ", LogType.ERROR);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        LogEvent("Ошибка при чтении часовых значений по ЭКОМ [" + e.Message + "]", LogType.ERROR);
-                    }
+										if (Fields.ContainsKey("Hour." + name))
+										{
+											Fields["Hour." + name].Value = Convert.ToSingle(Fields["Hour." + name].Value) + value;
+											Fields["Hour." + name].Quality = 192;
+										}
+									}
+								}
 
-                    if (date.Hour == 0 || firstStart)
-                    {
-                        date = date.AddDays(-1);
-                        url = Configuration.Url + "&interval=day" + "&t1=" + date.ToString("yyyyMMdd000000.000") + "&t2=" + date.ToString("yyyyMMdd235959.000");
+								Fields["IsActive"].Value = true;
+								Fields["IsActive"].Quality = 192;
+							}
+						}
+						else
+						{
+							LogEvent("Нет часовых значений по ЭКОМ", LogType.ERROR);
 
-                        try
-                        {
-                            string raw = GetHtml(url);
+							lock (Fields)
+							{
+								Fields["IsActive"].Value = false;
+								Fields["IsActive"].Quality = 192;
+							}
+						}
+					}
+					catch (Exception e)
+					{
+						LogEvent("Ошибка при чтении часовых значений по ЭКОМ [" + e.Message + "]", LogType.ERROR);
 
-                            if (!string.IsNullOrEmpty(raw))
-                            {
-                                lock (Fields)
-                                {
-                                    string[] rows = raw.Split('\n');
+						lock (Fields)
+						{
+							Fields["IsActive"].Value = false;
+							Fields["IsActive"].Quality = 192;
+						}
+					}
 
-                                    foreach (var key in Fields.Keys)
-                                        if (key.Contains("Day.")) Fields[key].Value = 0;
+					if (date.Hour == 0 || firstStart)
+					{
+						date = date.AddDays(-1);
+						url = Configuration.Url + "&interval=day" + "&t1=" + date.ToString("yyyyMMdd000000.000") + "&t2=" + date.ToString("yyyyMMdd235959.000");
 
-                                    foreach (string row in rows)
-                                    {
-                                        string[] parts = row.Split(',');
+						try
+						{
+							string raw = GetHtml(url);
 
-                                        if (parts.Length > 2)
-                                        {
-                                            string name = parts[0].Trim();
-                                            float value = float.TryParse(parts[2].Trim().Replace(".", ","), out float f) ? f : 0;
+							if (!string.IsNullOrEmpty(raw))
+							{
+								lock (Fields)
+								{
+									string[] rows = raw.Split('\n');
 
-                                            if (Fields.ContainsKey("Day." + name))
-                                            { 
-                                                Fields["Day." + name].Value = value; 
-                                                Fields["Day." + name].Quality = 192;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                LogEvent("Нет суточных значений по ЭКОМ", LogType.ERROR);
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            LogEvent("Ошибка при суточных часовых значений по ЭКОМ [" + e.Message + "]", LogType.ERROR);
-                        }
-                    }
-                }
+									foreach (var key in Fields.Keys)
+										if (key.Contains("Day.")) Fields[key].Value = 0;
 
-                UpdateEvent();
-            }
-            catch (Exception e)
-            {
-                LogEvent("Ошибка при получении данных [" + e.Message + "]", LogType.ERROR);
-            }
+									foreach (string row in rows)
+									{
+										string[] parts = row.Split(',');
 
-            Fields["Time"].Value = date.ToString("dd.MM.yyyy HH:mm:ss");
-            Fields["Time"].Quality = 192;
-        }
+										if (parts.Length > 2)
+										{
+											string name = parts[0].Trim();
+											float value = float.TryParse(parts[2].Trim().Replace(".", ","), out float f) ? f : 0;
 
-        string GetHtml(string url)
-        {
-            try
-            {
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-                req.Method = "GET";
-                req.Timeout = 10000;
-                req.Credentials = new NetworkCredential("modbus", "1");
+											if (Fields.ContainsKey("Day." + name))
+											{ 
+												Fields["Day." + name].Value = value; 
+												Fields["Day." + name].Quality = 192;
+											}
+										}
+									}
 
-                WebResponse res = req.GetResponse();
-                StreamReader sr = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+									Fields["IsActive"].Value = true;
+									Fields["IsActive"].Quality = 192;
+								}
+							}
+							else
+							{
+								LogEvent("Нет суточных значений по ЭКОМ", LogType.ERROR);
 
-                string raw = sr.ReadToEnd();
+								lock (Fields)
+								{
+									Fields["IsActive"].Value = false;
+									Fields["IsActive"].Quality = 192;
+								}
+							}
+						}
+						catch (Exception e)
+						{
+							LogEvent("Ошибка при суточных часовых значений по ЭКОМ [" + e.Message + "]", LogType.ERROR);
+							
+							lock (Fields)
+							{
+								Fields["IsActive"].Value = false;
+								Fields["IsActive"].Quality = 192;
+							}
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				LogEvent("Ошибка при получении данных [" + e.Message + "]", LogType.ERROR);
 
-                sr.Close();
-                res.Close();
+				lock (Fields)
+				{
+					Fields["IsActive"].Value = false;
+					Fields["IsActive"].Quality = 192;
+				}
+			}
+			finally
+			{
+				lock (Fields)
+				{
+					Fields["Time"].Value = date.ToString("dd.MM.yyyy HH:mm:ss");
+					Fields["Time"].Quality = 192;
+				}
 
-                Console.WriteLine("Url: " + url + "\nRaw: " + raw);
+				UpdateEvent();
+			}
+		}
 
-                return raw;
-            }
-            catch (Exception e)
-            {
-                LogEvent("Ошибка при HTTP запросе к источнику данных: " + e.Message, LogType.ERROR);
-                return null;
-            }
-        }
-    }
+		string GetHtml(string url)
+		{
+			try
+			{
+				HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+				req.Method = "GET";
+				req.Timeout = 10000;
+				req.Credentials = new NetworkCredential("modbus", "1");
+
+				WebResponse res = req.GetResponse();
+				StreamReader sr = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+
+				string raw = sr.ReadToEnd();
+
+				sr.Close();
+				res.Close();
+
+				Console.WriteLine("Url: " + url + "\nRaw: " + raw);
+
+				return raw;
+			}
+			catch (Exception e)
+			{
+				LogEvent("Ошибка при HTTP запросе к источнику данных: " + e.Message, LogType.ERROR);
+				return null;
+			}
+		}
+	}
 }
