b39686d6a5847550c37a239bc9a00b8630ffbc48
Юрий Дулин <y.dulin@vitebsk.energo.net>
2023-02-03 10:04:49 +0300

    [Add] Драйвер для разбора бинарных файлов систем АСК

---
+++ Drivers/ASK_Binary/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using iNOPC.Library;
+using Newtonsoft.Json;
+
+namespace iNOPC.Drivers.ASK_Binary
+{
+	public class Configuration
+	{
+		public int Timeout { get; set; } = 10000;
+
+		public string Path { get; set; } = "";
+
+		public static string GetPage(string json)
+		{
+			var config = JsonConvert.DeserializeObject<Configuration>(json);
+
+			return
+				Html.Value("Интервал опроса, мин", nameof(config.Timeout), config.Timeout) +
+				Html.Value("Путь к папке с файлами", nameof(config.Path), config.Path) +
+				"";
+		}
+	}
+}
