3017ff1d29dd5c2cae585c1d56321018beef65c3
yura.dulin <y.dulin@vitebsk.energo.net>
2023-04-13 12:36:34 +0300

    [Add] Проект сборщика сообщений

---
+++ Logger/Logger/Logs/LogsWorker.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Logger.Logs
+{
+	public static class LogsWorker
+	{
+		public static async void Start(CancellationToken token)
+		{
+			while (!token.IsCancellationRequested)
+			{
+				await Task.Delay(1000);
+			}
+		}
+	}
+}
---
+++ Logger/Logger/Web/Api/Default.cs
@@ -0,0 +1,13 @@
+﻿using Logger.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Web.Api
+{
+	internal class Default : Controller
+	{
+	}
+}
