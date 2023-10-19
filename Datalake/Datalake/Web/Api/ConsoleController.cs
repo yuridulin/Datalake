eb5d18f87cfbae8ef5bd7cc3533a35c8d0d5f121
yura.dulin <y.dulin@vitebsk.energo.net>
2023-10-19 16:06:15 +0300

    [Add] Метод для получения дерева объектов

---
+++ Datalake/Datalake/Web/Api/ConsoleController.cs
@@ -0,0 +1,95 @@
+﻿using Datalake.Database;
+using Datalake.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Datalake.Web.Api
+{
+	public class ConsoleController : Controller
+	{
+		public object Tree()
+		{
+			using (var db = new DatabaseContext())
+			{
+				var tags = db.Tags
+					.ToList();
+
+				var sources = db.Sources
+					.ToList();
+
+				var tree = sources
+					.Select(x => new TreeItem
+					{
+						Id = x.Id,
+						Name = x.Name,
+						Type = TreeType.Source,
+						Items = GetSubItems(tags
+							.Where(t => t.SourceId == x.Id)
+							.Select(t => new Tag
+							{
+								Id = t.Id,
+								Name = t.Name.Replace(x.Name + '.', ""),
+							})
+							.ToList()),
+					})
+					.ToList();
+
+				var withoutSource = tags
+					.Where(t => !sources.Select(x => x.Id).Contains(t.SourceId))
+					.ToList();
+
+				if (withoutSource.Count > 0)
+				{
+					tree.Add(new TreeItem
+					{
+						Id = 0,
+						Name = "Источник не найден",
+						Type = TreeType.Source,
+						Items = GetSubItems(withoutSource),
+					});
+				}
+
+				return tree;
+			}
+
+			List<TreeItem> GetSubItems(List<Tag> tags)
+			{
+				var subs =  tags
+					.Select(x => x.Name.Contains('.') ? x.Name.Substring(0, x.Name.IndexOf('.')) : x.Name)
+					.Distinct()
+					.Select(x => new TreeItem
+					{
+						Name = x,
+					})
+					.ToList();
+
+				foreach (var item in subs)
+				{
+					var included = tags
+						.Where(x => x.Name.StartsWith(item.Name + '.'))
+						.Select(x => new Tag
+						{
+							Id = x.Id,
+							Name = x.Name.Replace(item.Name + '.', ""),
+						})
+						.ToList();
+
+					if (included.Count == 0)
+					{
+						item.Id = tags.FirstOrDefault(x => x.Name == item.Name).Id;
+						item.Type = TreeType.Tag;
+					}
+					else
+					{
+						item.Type = TreeType.TagGroup;
+					}
+
+					item.Items = GetSubItems(included);
+				}
+
+				return subs;
+			}
+		}
+	}
+}
---
+++ Datalake/Datalake/Web/Models/TreeItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Datalake.Web.Models
+{
+	public class TreeItem
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public TreeType Type { get; set; }
+
+		public List<TreeItem> Items { get; set; } = new List<TreeItem>();
+	}
+
+	public enum TreeType
+	{
+		Source,
+		TagGroup,
+		Tag,
+		Block,
+		Link,
+	}
+}
