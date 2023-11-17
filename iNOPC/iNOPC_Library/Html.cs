ecb551297e9967dc7c46c18dc4596f2f99b0c744
yura.dulin <y.dulin@vitebsk.energo.net>
2023-11-17 14:28:16 +0300

    [Edit] Стилизация для элементов ввода

--- iNOPC/iNOPC_Library/Html.cs
+++ iNOPC/iNOPC_Library/Html.cs
@@ -2,53 +2,50 @@
 {
 	public static class Html
 	{
-		public static string Value(string desc, string name, object value)
-		{
-			return "<div type='value'>" + Input(desc, name, value) + "</div>";
-		}
+		public static string Value(string desc, string name, object value) 
+			=> $"<div type='value'>{Input(desc, name, value)}</div>";
 
-		public static string Input(string desc, string name, object value, int percentWidth = 0)
+		public static string Input(string desc, string name, object value, string style = "")
 		{
 			var type = value.GetType();
-
-			string style = percentWidth == 0 ? "" : "style='width: " + percentWidth + "%;'";
 
 			// Одиночные кавычки, иначе разрывается строка в исполняемой js функции. Приходящие строки тоже нужно экранировать, если там есть двойные кавычки.
 			// Можно добавить ``. Это поможет, но не будет работать в некоторых старых браузерах
 			if (type.Equals(typeof(byte)) || type.Equals(typeof(int)) || type.Equals(typeof(uint)) || type.Equals(typeof(ushort)) || type.Equals(typeof(float)))
 			{
-				return "<span>" + desc + "</span><input name='" + name + "' type='number' value='" + value.ToString().Replace(",", ".") + "' " + style + " />";
+				return $"<span>{desc}</span><input name='{name}' type='number' value='{value.ToString().Replace(",", ".")}' style='{style}' />";
 			}
 
-			if (type.Equals(typeof(bool)))
+			else if (type.Equals(typeof(bool)))
 			{
-				return "<span>" + desc + "</span><input name='" + name + "' type='checkbox' " + ((bool)value ? "checked" : "") + " " + style + " />";
+				return $"<span>{desc}</span><input name='{name}' type='checkbox' {((bool)value ? "checked" : "")} style='{style}' />";
 			}
-
-			return "<span>" + desc + "</span><input name='" + name + "' type='text' value='" + value + "' " + style + " />";
+			else
+			{
+				return $"<span>{desc}</span><input name='{name}' type='text' value='{value}' style='{style}' />";
+			}
 		}
 
-		public static string Textarea(string desc, string name, object value)
+		public static string Textarea(string desc, string name, object value, string style = "")
 		{
-			return "<div type='value'><span>" + desc + "</span><textarea name='" + name + "'>" + value + "</textarea></div>";
+			return $"<div type='value'><span>{desc}</span><textarea name='{name}' style='{style}'>{value}</textarea></div>";
 		}
 
-		public static string V(string name, object value, string width = null)
+		public static string V(string name, object value, string style = "")
 		{
 			var type = value.GetType();
-			string w = width == null ? "" : " style='width: " + width + "' ";
 
 			if (type.Equals(typeof(byte)) || type.Equals(typeof(int)) || type.Equals(typeof(uint)) || type.Equals(typeof(ushort)))
 			{
-				return "<input v name='" + name + "' type='number' value='" + value + "' " + w + "/>";
+				return $"<input v name='{name}' type='number' value='{value}' style='{style}'/>";
 			}
 
-			if (type.Equals(typeof(bool)))
+			else if (type.Equals(typeof(bool)))
 			{
-				return "<input v name='" + name + "' type='checkbox' " + ((bool)value ? "checked" : "") + " " + w + "/>";
+				return $"<input v name='{name}' type='checkbox' {((bool)value ? "checked" : "")} style='{style}'/>";
 			}
 
-			return "<input v name='" + name + "' type='text' value='" + value + "' " + w + "/>";
+			else return $"<input v name='{name}' type='text' value='{value}' style='{style}'/>";
 		}
 	}
 }
