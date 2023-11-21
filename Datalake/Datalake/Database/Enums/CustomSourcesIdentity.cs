a603ddf69149986dd82ae1d70e7368655828f64b
yura.dulin <y.dulin@vitebsk.energo.net>
2023-11-21 11:14:42 +0300

    [Add] Идентификаторы псевдо-источников

---
+++ Datalake/Datalake/Database/Enums/CustomSourcesIdentity.cs
@@ -0,0 +1,11 @@
+﻿namespace Datalake.Database.Enums
+{
+	public static class CustomSourcesIdentity
+	{
+		public static readonly int Unset = 0;
+
+		public static readonly int Calculated = -1;
+
+		public static readonly int Manual = -2;
+	}
+}
