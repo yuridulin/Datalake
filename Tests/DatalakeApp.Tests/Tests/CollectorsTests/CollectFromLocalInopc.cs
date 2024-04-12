4bdd66abb217a5db3de79dc2d58b576ec321aa51
Юрий Дулин <y.dulin@vitebsk.energo.net>
2024-04-12 15:14:13 +0300

    [Tests][Add] Тестирование получения данных из iNOPC

---
+++ Tests/DatalakeApp.Tests/Tests/CollectorsTests/CollectFromLocalInopc.cs
@@ -0,0 +1,157 @@
+﻿using DatalakeApp.Services.Receiver.Models.Inopc;
+using DatalakeApp.Tests.Attributes;
+using DatalakeApp.Tests.Extensions;
+using DatalakeDatabase.ApiModels.Sources;
+using DatalakeDatabase.ApiModels.Tags;
+using DatalakeDatabase.ApiModels.Values;
+using DatalakeDatabase.Enums;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+
+namespace DatalakeApp.Tests.Tests.CollectorsTests;
+
+public class CollectFromLocalInopc : IClassFixture<TestingWebAppFactory<Program>>
+{
+	private readonly TestingWebAppFactory<Program> _webAppFactory;
+	private readonly ILogger<CollectFromLocalInopc> _logger;
+	private readonly HttpClient _httpClient;
+
+	public CollectFromLocalInopc(TestingWebAppFactory<Program> webAppFactory)
+	{
+		_webAppFactory = webAppFactory;
+		_logger = new LoggerFactory().CreateLogger<CollectFromLocalInopc>();
+		_httpClient = _webAppFactory.CreateDefaultClient();
+	}
+
+
+	const string LocalInopcIp = "10.208.4.113";
+	const string LocalInopcUrl = $"http://{LocalInopcIp}:81/api/storage/read";
+	const string TestSourceName = "LocalInopcTest";
+	const string TestItemPath = "Test.TestDevice.Num1";
+	const string TestTagName = "TestTag1";
+
+	[Fact, Priority(0)]
+	public void Prepare()
+	{
+		Assert.True(true);
+	}
+
+	[Fact, Priority(1)]
+	public async Task LocalInopcMustBeAvailable()
+	{
+		var request = new InopcRequest
+		{
+			Tags = []
+		};
+
+		var globalHttpClient = new HttpClient();
+		var response = await globalHttpClient.PostAsync(LocalInopcUrl, JsonContent.Create(request));
+
+		Assert.True(response.IsSuccessStatusCode);
+	}
+
+	[Fact, Priority(2)]
+	public async Task CreateSourceForLocalInopc()
+	{
+		var body = new SourceInfo
+		{
+			Name = TestSourceName,
+			Type = SourceType.Inopc,
+			Address = LocalInopcIp,
+		};
+
+		int sourceId = await _httpClient.PostAsync<int>("api/sources", body);
+		Assert.True(sourceId > 0);
+	}
+
+	[Fact, Priority(3)]
+	public async Task GetItemsListForTestSource()
+	{
+		var sources = await _httpClient.GetAsync<SourceInfo[]>("api/sources");
+		Assert.NotNull(sources);
+
+		var testSource = sources.FirstOrDefault(x => x.Name == TestSourceName);
+		Assert.NotNull(testSource);
+
+		var items = await _httpClient.GetAsync<SourceEntryInfo[]>($"api/sources/{testSource.Id}/tags");
+		Assert.True(items.Length >= 3);
+		Assert.Contains(items, x => x.ItemInfo?.Path == TestItemPath);
+	}
+
+	[Fact, Priority(4)]
+	public async Task CreateTestTagForTestSource()
+	{
+		var sources = await _httpClient.GetAsync<SourceInfo[]>("api/sources");
+		Assert.NotNull(sources);
+
+		var testSource = sources.FirstOrDefault(x => x.Name == TestSourceName);
+		Assert.NotNull(testSource);
+
+		var id = await _httpClient.PostAsync<int>("api/tags", new TagInfo
+		{
+			Name = TestTagName,
+			IntervalInSeconds = 1,
+			SourceInfo = new TagInfo.TagSourceInfo
+			{
+				Id = testSource.Id,
+				Item = TestItemPath,
+			}
+		});
+
+		Assert.True(id > 0);
+	}
+
+	[Fact, Priority(5)]
+	public async Task EnsureValueOfTestTagsUpdatesTwice()
+	{
+		await Task.Delay(TimeSpan.FromSeconds(1));
+
+		object? storedValue = null;
+
+		for (var i = 0; i < 4; i++)
+		{
+			var request = new ValuesRequest[]
+			{
+				new()
+				{
+					TagNames = [TestTagName]
+				}
+			};
+
+			var responses = await _httpClient.PostAsync<List<ValuesResponse>>("api/tags/values", request);
+			Assert.NotNull(responses);
+			Assert.Single(responses);
+
+			var currentValues = responses[0].Values;
+			Assert.NotNull(currentValues);
+			Assert.Single(currentValues);
+
+			var currentValue = currentValues[0];
+			Assert.NotNull(currentValue);
+			Assert.False(currentValue.Value == storedValue);
+
+			storedValue = currentValue.Value;
+			await Task.Delay(TimeSpan.FromSeconds(1));
+		}
+	}
+
+	[Fact, Priority(int.MaxValue)]
+	public async Task RemoveTestObjects()
+	{
+		var sources = await _httpClient.GetAsync<SourceInfo[]>("api/sources");
+		Assert.NotNull(sources);
+
+		var testSource = sources.FirstOrDefault(x => x.Name == TestSourceName);
+		Assert.NotNull(testSource);
+
+		await _httpClient.DeleteAsync("api/sources/" + testSource.Id);
+
+		var tags = await _httpClient.GetAsync<TagInfo[]>("api/tags");
+		Assert.NotNull(tags);
+
+		var testTag = tags.FirstOrDefault(x => x.Name == TestTagName);
+		Assert.NotNull(testTag);
+
+		await _httpClient.DeleteAsync("api/tags/" + testTag.Id);
+	}
+}
