13ba1484b507738f9f4671badf6757ff1d364dab
Юрий Дулин <y.dulin@vitebsk.energo.net>
2024-06-11 17:00:13 +0300

    [Edit] Прокидывание переменных окружения в строку подключения к БД

--- Server/Program.cs
+++ Server/Program.cs
@@ -19,6 +19,8 @@ using NJsonSchema.Generation;
 using Serilog;
 using System.Reflection;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
+
 #if DEBUG
 using LinqToDB.AspNet.Logging;
 #endif
@@ -54,7 +56,6 @@ namespace DatalakeServer
 			builder.Services.AddEndpointsApiExplorer();
 
 			ConfigureDatabase(builder);
-			//ConfigureAuth(builder);
 			ConfigureServices(builder);
 
 			var app = builder.Build();
@@ -73,8 +74,6 @@ namespace DatalakeServer
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
 			app.UseRouting();
-			//app.UseAuthentication();
-			//app.UseAuthorization();
 			app.UseCors(policy =>
 			{
 				policy
@@ -103,7 +102,17 @@ namespace DatalakeServer
 
 		static void ConfigureDatabase(WebApplicationBuilder builder)
 		{
-			var connectionString = builder.Configuration.GetConnectionString("Default");
+			var connectionString = builder.Configuration.GetConnectionString("Default") ?? "";
+
+			var env = Environment.GetEnvironmentVariables();
+			foreach (var key in env.Keys)
+			{
+				var arg = "${" + key + "}";
+				if (connectionString.Contains(arg))
+				{
+					connectionString = connectionString.Replace(arg, env[key]?.ToString() ?? arg);
+				}
+			}
 
 			builder.Services.AddDbContext<DatalakeEfContext>(options =>
 			{
@@ -122,53 +131,6 @@ namespace DatalakeServer
 					.UseDefaultLogging(provider)
 #endif
 			);
-		}
-
-		static void ConfigureAuth(WebApplicationBuilder builder)
-		{
-			builder.Services
-				.AddAuthentication(options =>
-				{
-					options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-					options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-				})
-				.AddCookie(options =>
-				{
-					options.LoginPath = "/account/login";
-					options.AccessDeniedPath = "/account/noAccess";
-					options.LogoutPath = "/account/logout";
-				})
-				.AddOpenIdConnect(options =>
-				{
-					options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-					options.Authority = "http://localhost:9090/realms/energo/";
-					options.ClientId = "datalake";
-					options.ClientSecret = "8q9UuxZplUNgusxx7dnGZl3xTP7Ce56q";
-					options.MetadataAddress = "http://localhost:9090/realms/energo/.well-known/openid-configuration";
-					options.RequireHttpsMetadata = false;
-					options.GetClaimsFromUserInfoEndpoint = true;
-					options.Scope.Add("openid");
-					options.Scope.Add("profile");
-					options.SaveTokens = true;
-					options.ResponseType = OpenIdConnectResponseType.Code;
-					options.NonceCookie.SameSite = SameSiteMode.Unspecified;
-					options.CorrelationCookie.SameSite = SameSiteMode.Unspecified;
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						NameClaimType = "name",
-						RoleClaimType = ClaimTypes.Role,
-						ValidateIssuer = true
-					};
-				});
-
-			builder.Services
-				.AddAuthorizationBuilder()
-					.AddPolicy("user", policy =>
-						policy.RequireAssertion(context =>
-							context.User.HasClaim(c => c.Type == "realm_access" && c.Value.Contains("datalake_user"))))
-					.AddPolicy("admin", policy =>
-						policy.RequireAssertion(context =>
-							context.User.HasClaim(c => c.Type == "realm_access" && c.Value.Contains("datalake_admin"))));
 		}
 
 		static void ConfigureServices(WebApplicationBuilder builder)
