2c886784797ab32852edb23bf260fe32108a490f
Юрий Дулин <u.dulin@vitebsk.energo.net>
2024-10-09 14:49:25 +0300

    [Add] Кнопка перезапуска сбора данных

--- Client/datalake/src/app/pages/admin/tabs/CollectSettings.tsx
+++ Client/datalake/src/app/pages/admin/tabs/CollectSettings.tsx
@@ -1,3 +1,19 @@
+import { Button, Space, Spin } from 'antd'
+import { useState } from 'react'
+import api from '../../../../api/swagger-api'
+
 export default function CollectSettings() {
-	return <></>
+	const [wait, setWait] = useState(false)
+	const restart = () => {
+		setWait(true)
+		api.configRestart().then((res) => res.status === 204 && setWait(false))
+	}
+
+	return (
+		<Space onClick={restart}>
+			<Button disabled={wait}>
+				{wait && <Spin />} Перезапуск всех опросов
+			</Button>
+		</Space>
+	)
 }
