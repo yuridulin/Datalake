d508a962ea7fb43181ed3556202b46496a78f591
Юрий Дулин <u.dulin@vitebsk.energo.net>
2024-12-11 15:26:31 +0300

    [Add] Переход назад по истории для всех не-корневых страниц

--- Client/datalake/src/app/components/PageHeader.tsx
+++ Client/datalake/src/app/components/PageHeader.tsx
@@ -1,4 +1,8 @@
+import routes from '@/app/router/routes'
+import { ArrowLeftOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
 import { ReactNode } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 type HeaderProps = {
 	left?: ReactNode
@@ -7,6 +11,13 @@ type HeaderProps = {
 }
 
 export default function PageHeader({ left, children, right }: HeaderProps) {
+	const navigate = useNavigate()
+
+	const back = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+		e.preventDefault()
+		navigate(-1)
+	}
+
 	return (
 		<div style={{ display: 'table', width: '100%', marginBottom: '2em' }}>
 			{children && (
@@ -24,6 +35,15 @@ export default function PageHeader({ left, children, right }: HeaderProps) {
 								display: 'inline-block',
 							}}
 						>
+							<NavLink to={routes.globalRoot} onClick={back}>
+								<Button
+									icon={<ArrowLeftOutlined />}
+									title='Вернуться на предыдущую страницу'
+								>
+									Назад
+								</Button>
+							</NavLink>
+							&ensp;
 							{left}
 						</div>
 					)}
