ae3f0012c3c50101493573b786da07106294ee27
Юрий Дулин <u.dulin@vitebsk.energo.net>
2025-06-25 14:17:39 +0300

    [Add] Миграции для перехода на timescale

--- Database/Migrations/20250625111157_EndSwitchToTimescale.Designer.cs
+++ Database/Migrations/20250625111157_EndSwitchToTimescale.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Datalake.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Datalake.Database.Migrations
 {
     [DbContext(typeof(DatalakeEfContext))]
-    partial class DatalakeEfContextModelSnapshot : ModelSnapshot
+    [Migration("20250625111157_EndSwitchToTimescale")]
+    partial class EndSwitchToTimescale
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Database/Migrations/20250625111157_EndSwitchToTimescale.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datalake.Database.Migrations
+{
+	/// <inheritdoc />
+	public partial class EndSwitchToTimescale : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql(@"CREATE EXTENSION IF NOT EXISTS timescaledb;");
+
+			migrationBuilder.Sql(@"SELECT create_hypertable('""TagsHistory""', by_range('Date', INTERVAL '1 day'));");
+
+			migrationBuilder.Sql(@"
+				ALTER TABLE ""TagsHistory""
+				ALTER COLUMN ""Date"" TYPE timestamptz
+				USING ""Date"" AT TIME ZONE 'Europe/Moscow';");
+
+			migrationBuilder.Sql(@"
+				DO $$
+				DECLARE
+						tbl text;
+				BEGIN
+						FOR tbl IN
+								SELECT quote_ident(tablename)
+								FROM pg_tables
+								WHERE tablename ~ '^TagsHistory_\d{4}_\d{2}_\d{2}$'
+						LOOP
+								EXECUTE format(
+										'INSERT INTO ""TagsHistory"" (""TagId"", ""Date"", ""Text"", ""Number"", ""Quality"")
+										 SELECT ""TagId"", ""Date"" AT TIME ZONE ''Europe/Moscow'', ""Text"", ""Number"", ""Quality""
+										 FROM %s',
+										tbl
+								);
+						END LOOP;
+				END;
+				$$;");
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+
+		}
+	}
+}
