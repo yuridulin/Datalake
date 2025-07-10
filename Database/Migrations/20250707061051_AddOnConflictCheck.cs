3b39a57360bf867e9b3f41013afed713e22534b2
Юрий Дулин <u.dulin@vitebsk.energo.net>
2025-07-10 14:35:04 +0300

    [Fix] Забыл раскомментировать

--- Database/Migrations/20250707061051_AddOnConflictCheck.cs
+++ Database/Migrations/20250707061051_AddOnConflictCheck.cs
@@ -10,7 +10,7 @@ namespace Datalake.Database.Migrations
 		/// <inheritdoc />
 		protected override void Up(MigrationBuilder migrationBuilder)
 		{
-			/*migrationBuilder.Sql(@"
+			migrationBuilder.Sql(@"
 					DELETE FROM ""TagsHistory"" th
 					USING (
 							SELECT MIN(ctid) AS keep_ctid, ""TagId"", ""Date""
@@ -21,40 +21,40 @@ namespace Datalake.Database.Migrations
 					WHERE th.""TagId"" = dup.""TagId""
 						AND th.""Date"" = dup.""Date""
 						AND th.ctid <> dup.keep_ctid;
-					");*/
+					");
 
-			/*migrationBuilder.Sql(@"
+			migrationBuilder.Sql(@"
 					-- Удаляем существующий неуникальный индекс
 					DROP INDEX IF EXISTS ""TagsHistory_TagId_Date_idx"";
 
 					-- Создаём уникальный индекс с нужным порядком сортировки
 					CREATE UNIQUE INDEX ""TagsHistory_TagId_Date_idx""
-					ON ""TagsHistory"" (""TagId"" ASC, ""Date"" DESC);");*/
+					ON ""TagsHistory"" (""TagId"" ASC, ""Date"" DESC);");
 
-			/*migrationBuilder.CreateIndex(
+			migrationBuilder.CreateIndex(
 					name: "TagsHistory_TagId_Date_idx",
 					schema: "public",
 					table: "TagsHistory",
 					columns: new[] { "TagId", "Date" },
 					unique: true,
-					descending: new[] { false, true });*/
+					descending: new[] { false, true });
 		}
 
 		/// <inheritdoc />
 		protected override void Down(MigrationBuilder migrationBuilder)
 		{
-			/*migrationBuilder.Sql(@"
+			migrationBuilder.Sql(@"
 					-- Удаляем существующий уникальный индекс
 					DROP INDEX IF EXISTS ""TagsHistory_TagId_Date_idx"";
 
 					-- Создаём неуникальный индекс с нужным порядком сортировки
 					CREATE INDEX ""TagsHistory_TagId_Date_idx""
-					ON ""TagsHistory"" (""TagId"" ASC, ""Date"" DESC);");*/
+					ON ""TagsHistory"" (""TagId"" ASC, ""Date"" DESC);");
 
-			/*migrationBuilder.DropIndex(
+			migrationBuilder.DropIndex(
 					name: "TagsHistory_TagId_Date_idx",
 					schema: "public",
-					table: "TagsHistory");*/
+					table: "TagsHistory");
 		}
 	}
 }
